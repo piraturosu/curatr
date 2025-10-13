d23e00bc4794622db43bc14318ca5c724c9eae52
George Constantin <george.constantin33@gmail.com>
2025-10-13 11:31:47 +0100

    be: fix render deploy

--- backend/listen.js
+++ backend/listen.js
@@ -1,5 +1,5 @@
-const { app } = require("./app.js");
-const { PORT = 8080 } = process.env;
+const app = require("./app.js");
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("listening to port 8080");
